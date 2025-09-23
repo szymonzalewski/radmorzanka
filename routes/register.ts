644e75fd5976ea77ad3ee87110fc8b3b194b0456
Szymon Zalewski <szymon_zalewski@macbook-pro-szymon.home>
2025-09-23 23:17:50 +0200

    User registration admin token

--- routes/register.ts
+++ routes/register.ts
@@ -1,26 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const users = require("./usersShare");
 const db = require("../db");
-
 const bcrypt = require("bcrypt");
 
 router.post("/", async (req: any, res: any) => {
-  const { firstname, surname, email, password, role } = req.body;
+  const { firstname, surname, email, password_hash, role } = req.body;
 
-  const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password_hash, 10);
+  const user = await db.one(
+    "INSERT INTO users (firstname, surname, email, password_hash, role) VALUES ($1, $2, $3, $4, $5) RETURNING id, firstname, surname, email, role, created_at;",
+    [firstname, surname, email, hashedPassword, role]
+  );
 
-  const user = {
-    id: users.length + 1,
-    firstname,
-    surname,
-    email,
-    password: hashedPassword,
-    role,
-  };
-  users.push(user);
-
-  res.send(user);
+  res.json({ user });
 });
 
 module.exports = router;
